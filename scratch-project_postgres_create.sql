f763538c7a8e0d2261ef7e55224dcb743f95325d
lu0713 <lumie.song@gmail.com>
2020-08-15 20:25:24 -0400

    debugging

--- scratch-project_postgres_create.sql
+++ scratch-project_postgres_create.sql
@@ -8,21 +8,21 @@ CREATE TABLE users
   UNIQUE ( username )
 );
 
-INSERT INTO users
-  (username, firstname, lastname, profilephoto)
-VALUES('bonjay123', 'bonjay', 'tseng', 'photo TBD');
+-- INSERT INTO users
+--   (username, firstname, lastname, profilephoto)
+-- VALUES('bonjay123', 'bonjay', 'tseng', 'photo TBD');
 
-INSERT INTO users
-  (username, firstname, lastname, profilephoto)
-VALUES('minchan123', 'minchan', 'jun', 'photo TBD');
+-- INSERT INTO users
+--   (username, firstname, lastname, profilephoto)
+-- VALUES('minchan123', 'minchan', 'jun', 'photo TBD');
 
-INSERT INTO users
-  (username, firstname, lastname, profilephoto)
-VALUES('stella123', 'stella', 'song', 'photo TBD');
+-- INSERT INTO users
+--   (username, firstname, lastname, profilephoto)
+-- VALUES('stella123', 'stella', 'song', 'photo TBD');
 
-INSERT INTO users
-  (username, firstname, lastname, profilephoto)
-VALUES('marc123', 'marc', 'burnie', 'photo TBD');
+-- INSERT INTO users
+--   (username, firstname, lastname, profilephoto)
+-- VALUES('marc123', 'marc', 'burnie', 'photo TBD');
 
 SELECT setval('users_userid_seq', 1, false);
 -- SELECT setval('users_userid_seq', max(userid))
@@ -37,28 +37,38 @@ CREATE TABLE events
   "eventendtime" time NOT NULL,
   "eventlocation" varchar NOT NULL CHECK ( eventlocation  <> ''),
   "eventdetails" varchar NOT NULL CHECK ( eventdetails  <> ''),
-  "eventowner" varchar NOT NULL,
-  UNIQUE ( eventtitle ),
-  FOREIGN KEY (eventowner) REFERENCES users (username)
+  "eventownerid" bigint NOT NULL,
+  "eventownerusername" varchar NOT NULL,
+  UNIQUE ( eventtitle )
+  -- FOREIGN KEY (eventownerid) REFERENCES users (userid),
+  -- FOREIGN KEY (eventownerusername) REFERENCES users (username)
 );
 
-INSERT INTO events
-  (eventtitle, eventdate, eventstarttime, eventendtime, eventlocation, eventdetails, eventowner)
-VALUES('bonjay birthday', '8/29/2020', '07:00 PM', '08:00 PM', 'Central Park', 'birthday partayy', 'bonjay123');
+-- INSERT INTO events
+--   (eventtitle, eventdate, eventstarttime, eventendtime, eventlocation, eventdetails, eventownerid, eventownerusername)
+-- VALUES('bonjay birthday', '8/29/2020', '07:00 PM', '08:00 PM', 'Central Park', 'birthday partayy', 1, 'bonjay123');
 
-INSERT INTO events
-  (eventtitle, eventdate, eventstarttime, eventendtime, eventlocation, eventdetails, eventowner)
-VALUES('minchan birthday', '8/30/2020', '08:00 AM', '08:00 PM', 'Six Flags', 'birthday partayyyy', 'minchan123');
+-- INSERT INTO events
+--   (eventtitle, eventdate, eventstarttime, eventendtime, eventlocation, eventdetails, eventownerid, eventownerusername)
+-- VALUES('minchan birthday', '8/30/2020', '08:00 AM', '08:00 PM', 'Six Flags', 'birthday partayyyy', 2, 'minchan123');
 
-INSERT INTO events
-  (eventtitle, eventdate, eventstarttime, eventendtime, eventlocation, eventdetails, eventowner)
-VALUES('stella birthday', '8/31/2020', '05:00 PM', '08:00 PM', 'Dave & Buster', 'birthday partayyyyyyyy', 'stella123'
-);
+-- INSERT INTO events
+--   (eventtitle, eventdate, eventstarttime, eventendtime, eventlocation, eventdetails, eventownerid, eventownerusername)
+-- VALUES('stella birthday', '8/31/2020', '05:00 PM', '08:00 PM', 'Dave & Buster', 'birthday partayyyyyyyy', 3, 'stella123'
+-- );
 
-INSERT INTO events
-  (eventtitle, eventdate, eventstarttime, eventendtime, eventlocation, eventdetails, eventowner)
-VALUES('marc birthday', '9/1/2020', '02:00 PM', '08:00 PM', 'Mohegan Sun', 'birthday parteeee', 'marc123'
-);
+-- INSERT INTO events
+--   (eventtitle, eventdate, eventstarttime, eventendtime, eventlocation, eventdetails, eventownerid, eventownerusername)
+-- VALUES('stella wedding', '2/3/2021', '05:00 PM', '08:00 PM', 'Castle in Ireland', 'weddingggg', 3, 'stella123'
+-- );
+
+-- INSERT INTO events
+--   (eventtitle, eventdate, eventstarttime, eventendtime, eventlocation, eventdetails, eventownerid, eventownerusername)
+-- VALUES('marc birthday', '9/1/2020', '02:00 PM', '08:00 PM', 'Mohegan Sun', 'birthday parteeee', 4, 'marc123'
+-- );
+
+-- ALTER TABLE events ADD CONSTRAINT "events_fk0" FOREIGN KEY ("eventownerid") REFERENCES users("userid");
+-- ALTER TABLE events ADD CONSTRAINT "events_fk1" FOREIGN KEY ("eventownerusername") REFERENCES  users("username");
 
 SELECT setval('events_eventid_seq', 1, false);
 -- SELECT setval('events_eventid_seq', max(eventid))
@@ -67,22 +77,47 @@ SELECT setval('events_eventid_seq', 1, false);
 
 CREATE TABLE usersandevents
 (
-  uselessid serial PRIMARY KEY,
-  userid bigint REFERENCES users (userid),
-  username varchar REFERENCES users (username),
-  eventid bigint REFERENCES events (eventid),
-  eventtitle varchar REFERENCES events (eventtitle)
+  "uselessid" serial PRIMARY KEY,
+  "userid" bigint NOT NULL,
+  "username" varchar NOT NULL,
+  "eventid" bigint NOT NULL,
+  "eventtitle" varchar NOT NULL
+  -- FOREIGN KEY ( userid ) REFERENCES users ( userid ),
+  -- FOREIGN KEY ( username ) REFERENCES users ( username ),
+  -- FOREIGN KEY ( eventid ) REFERENCES events ( eventid ),
+  -- FOREIGN KEY ( eventtitle ) REFERENCES events ( eventtitle )
 );
 
-INSERT INTO usersandevents
-  (userid, username, eventid, eventtitle)
-VALUES(1, 'bonjay123', 2, 'minchan birthday');
+-- ALTER TABLE  usersandevents ADD CONSTRAINT "usersandevents_fk0" FOREIGN KEY ("userid") REFERENCES users("userid");
+-- ALTER TABLE  usersandevents ADD CONSTRAINT "usersandevents_fk1" FOREIGN KEY ("username") REFERENCES users("username");
+-- ALTER TABLE  usersandevents ADD CONSTRAINT "usersandevents_fk2" FOREIGN KEY ("eventid") REFERENCES events("eventid");
+-- ALTER TABLE  usersandevents ADD CONSTRAINT "usersandevents_fk3" FOREIGN KEY ("eventtitle") REFERENCES events("eventtitle");
 
-INSERT INTO usersandevents
-  (userid, username, eventid, eventtitle)
-VALUES(3, 'stella123', 2, 'minchan birthday');
+-- INSERT INTO usersandevents
+--   (userid, username, eventid, eventtitle)
+-- VALUES(1, 'bonjay123', 2, 'minchan birthday');
 
+-- INSERT INTO usersandevents
+--   (userid, username, eventid, eventtitle)
+-- VALUES(3, 'stella123', 2, 'minchan birthday');
+
+-- INSERT INTO usersandevents
+--   (userid, username, eventid, eventtitle)
+-- VALUES(3, 'stella123', 1, 'bonjay birthday');
+
+-- INSERT INTO usersandevents
+--   (userid, username, eventid, eventtitle)
+-- VALUES(3, 'stella123', 4, 'marc birthday');
 
 SELECT setval('usersandevents_uselessid_seq', 1, false);
 -- SELECT setval('events_eventid_seq', max(eventid))
 -- FROM events;
+
+
+-- SELECT MAX(userid) FROM users
+-- DELETE FROM users WHERE ID = (SELECT MAX(userid) FROM users)
+
+
+-- DROP TABLE USERSANDEVENTS;
+-- DROP TABLE EVENTS;
+-- DROP TABLE USERS;
--- server/utils/queries.js
+++ server/utils/queries.js
@@ -2,37 +2,75 @@ const db = require("../models/models.js"); // remove after testing
 
 const queries = {};
 
-// QUERY TO SELECT SPECIFIC USER
-queries.userInfo = `SELECT * FROM users WHERE user_id=$1`; // const values = [req.query.id]
 
-// QUERY TO SELECT SPECIFIC USER'S EVENTS
-queries.userEvents = `SELECT * FROM usersandevents WHERE user_id=$1`;
+// GRAB EVENT'S ATTENDEES
+queries.selectEventAttendees = `SELECT * FROM usersandevents WHERE eventid=$1`;
+
+// GET USER'S EVENTS
+queries.userEvents = `SELECT * FROM usersandevents WHERE userid=$1`;
+
+// GET ALL USER'S PERSONAL INFO
+queries.userInfo = `SELECT * FROM users WHERE userid=$1`; // const values = [req.query.id]
+
+// QUERY TO ADD USER
+queries.addUser = `
+INSERT INTO users
+  (username, firstname, lastname, profilephoto)
+VALUES($1, $2, $3, $4);
+`;
+
+// let addStella = ['stella123', 'stella', 'song', 'photo TBD'];
+// let addMarc = ['marc123', 'marc', 'burnie', 'photo TBD'];
+// db.query(queries.addUser, addStella);
+// db.query(queries.addUser, addMarc);
+
+
+// DELETE USER
+queries.deleteUser = `
+  DELETE FROM usersandevents WHERE userid=$1;
+  DELETE FROM events WHERE eventownerid=$1;
+  DELETE FROM users WHERE userid=$1;
+`;
 
 // QUERY FOR WHEN USER CREATES EVENT 
-let createEvent = `INSERT INTO events
-  (eventtitle, eventdate, eventstarttime, eventendtime, eventlocation, eventdetails, eventowner)
-VALUES($1, $2, $3, $4, $5, $6, $7)`;
+queries.createEvent = `
+INSERT INTO events
+  (eventtitle, eventdate, eventstarttime, eventendtime, eventlocation, eventdetails, eventownerid, eventownerusername)
+VALUES($1, $2, $3, $4, $5, $6, $7, $8);
+`;
 
-// QUERY FOR USERS ADDS THEMSELVES TO OTHER PEOPL'S EVENTS
-let addUserToEvent = `INSERT INTO usersandevents
+// let marcBirthday = ['marc birthday', '9/1/2020', '02:00 PM', '08:00 PM', 'Mohegan Sun', 'birthday parteeee', 4, 'marc123']
+// let stellaWedding = ['stella wedding', '2/3/2021', '05:00 PM', '08:00 PM', 'Castle in Ireland', 'weddingggg', 3, 'stella123']
+// db.query(queries.createEvent, marcBirthday);
+// db.query(queries.createEvent, stellaWedding);
+
+
+// ADDS ALL CURRENT EVENTS TO USERSANDEVENTS
+queries.addtoUsersAndEvents = `
+INSERT INTO usersandevents (userid, username, eventid, eventtitle)
+SELECT eventownerid, eventownerusername, eventid, eventitle FROM events;
+`;
+
+// QUERY TO DELETE EVENT
+queries.deleteEvent = `
+DELETE FROM usersandevents WHERE eventid=$1;
+DELETE FROM events WHERE eventid=$1;
+`;
+
+// QUERY FOR USERS ADDS THEMSELVES TO OTHER PEOPLE'S EVENTS
+queries.addUserToEvent = `INSERT INTO usersandevents
   (userid, username, eventid, eventtitle)
 VALUES($1, $2, $3, $4);`;
 
-let selectedLastUserCreated = 'SELECT MAX(userid) FROM users' // CORRECT
-let deleteLastUserCreated = 'DELETE FROM users WHERE ID = (SELECT MAX(userid) FROM users)' // correct
 
 
 
 
-
-db.query(selectedLastUserCreated).then(data => console.log(data.rows[0]));
-
-
-
-
-
-// queries.query3 = `INSERT INTO usersandevents
-// VALUES(${nextEvent}, 1, 'bonjay123', 1, 'stella Birthday');`;
-// const values = [req.query]
+// CLEAR ALL TABLES & DATA
+queries.clearAll = `
+DROP TABLE usersandevents;
+DROP TABLE events;
+DROP TABLE users;
+`;
 
 module.exports = queries;
